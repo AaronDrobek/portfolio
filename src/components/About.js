61c4f09b8824b92da904d77412f4890c8e8e6cae
Aaron Drobek <adrobek09@gmail.com>
2017-09-20 16:57:18 -0400

    added some styling to about and portfolio pages

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ export default class About extends Component{
   render(){
     return(
       <div className="about_box">
+        <div>
       <h1> this is about </h1>
+        </div>
       </div>
     )
   }
